8ffb0ac52534bc4373453bc3fdc1f93d052d2f3c
Henrik Hellerøy <henrik.helleroy@bekk.no>
2016-08-23 16:02:25 +0200

    Add Spring Boot app with endpoints for health,metrics etc.

---
+++ src/main/java/no/bekk/workshop/BeanstalkWorkshopApplication.java
@@ -0,0 +1,12 @@
+package no.bekk.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstalkWorkshopApplication.class, args);
+	}
+}
