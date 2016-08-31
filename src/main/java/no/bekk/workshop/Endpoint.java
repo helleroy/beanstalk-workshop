7bf46369a88b16d6ed180a0186263ce4ba884226
Gustav Karlsson <gustav.karlsson@bekk.no>
2016-08-31 07:28:17 +0200

    Testing request draining

--- src/main/java/no/bekk/workshop/Endpoint.java
+++ src/main/java/no/bekk/workshop/Endpoint.java
@@ -1,6 +1,7 @@
 package no.bekk.workshop;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -17,11 +18,25 @@ public class Endpoint {
 
     @RequestMapping(name = "/hostname", path = "/hostname")
     public String hostname() {
+        return getHostname();
+    }
+
+    private String getHostname() {
         try {
             return InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException e) {
             throw new RuntimeException("Failed to resolve hostname", e);
         }
+    }
+
+    @RequestMapping(name = "/sleep", path = "/sleep")
+    public String sleep(@RequestParam(defaultValue = "10", name = "seconds") int sleepSeconds) {
+        try {
+            Thread.sleep(sleepSeconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted sleep", e);
+        }
+        return "request took " + sleepSeconds + "s on " + getHostname();
     }
 
 }
